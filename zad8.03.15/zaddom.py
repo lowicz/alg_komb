05c9925b89b9dba61b7bf501cb9d338739421673
lowicz <mc.lowicz@gmail.com>
2015-03-10 13:06:01 +0100

    Create zaddom.py

---
+++ zad8.03.15/zaddom.py
@@ -0,0 +1,17 @@
+""" Bijekcja w moim przykladzie, przyporzadkowuje
+kazdemu podzbiorowi ciag binarny, w ktorym 1 oznacza obecnosc
+elementu zbioru o tym samym indeksie""""
+
+n = int(raw_input("Podaj n: "))
+
+def next(s, n):
+    x = []
+    mask = 1
+    for i in xrange(n):
+        if mask & s > 0:
+            x.append(i+1)
+        mask = mask << 1
+    return x
+
+for s in xrange(2**n):
+    print next(s, n)
