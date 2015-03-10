ac75672d9a926d48a5ffe37c7a9b3638100f7db6
lowicz <mc.lowicz@gmail.com>
2015-03-10 21:43:48 +0100

    working version

--- zad8.03.15/zaddom.c
+++ zad8.03.15/zaddom.c
@@ -2,26 +2,27 @@
 
 void get_subset(int s, int n)
 {
-    unsigned int mask = 1;
-    for (int i = 0; i < n; ++i)
+    int mask = 1;
+    printf("{ ");
+    int i;
+    for (i = 1; i <= n; i++)
     {
-        printf("{ ");
-        if (mask & s > 0)
+        if ((mask & s) > 0)
         {
-            printf("%d, ", i+1);
+            printf("%d ", i);
         }
         mask = mask << 1;
-        printf("}\n");
     }
+    printf("}\n");
 }
 
-int main(int argc, char const *argv[])
+int main()
 {
     int n;
     printf("Podaj n: ");
     scanf("%d", &n);
-    unsigned int s;
-    for (s = 0; s < ((2^n) - 1); ++s)
+    int s;
+    for (s = 0; s < (1<<n); ++s)
     {
         get_subset(s, n);
     }
