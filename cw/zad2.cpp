7308852062de17f3df8048bf69ff16a37e3c9f4e
lowicz <s391382@pc24c.labs.wmi.amu.edu.pl>
2015-05-17 11:05:02 +0200

    stirling2 add

---
+++ cw/zad2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+
+int stirling2(int n, int k) {
+    int s[n+1][k+1];
+    
+     for (int j=0; j <= k; j++) {
+        for(int i=0; i <=n; i++) {
+            s[i][j] = 0;
+        }
+    }
+    
+    s[0][0] = 1;  
+   
+    
+    for (int j=1; j <= k; j++) {
+        for(int i=1; i <=n; i++) {
+            
+            s[i][j] = s[i-1][j-1] + j*s[i-1][j];
+        }
+    }
+    
+    return s[n][k];
+}
+
+int main()
+{
+    int n, k;
+    cout << "Podaj n: ";
+    cin >> n;
+    
+    cout << "Podaj k: ";
+    cin >> k;
+    
+    
+    
+    cout << stirling2(n,k) << endl;    
+    return 0;
+}
